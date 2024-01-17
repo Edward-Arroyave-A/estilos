696dd5aae7f078a97de909831de6b26b5acf12c0
Edward   Alexander Arroyave Agudelo <edward.arroyave@ithealth.co>
2024-01-17 14:22:18 -0500

    add: iconodcom port

--- Codigo/Forms/Resultados.Designer.cs
+++ Codigo/Forms/Resultados.Designer.cs
@@ -374,11 +374,11 @@
             // pictureBox1
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.White;
-            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.pictureBox1.Location = new System.Drawing.Point(23, 19);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(223, 50);
+            this.pictureBox1.Size = new System.Drawing.Size(54, 50);
             this.pictureBox1.TabIndex = 28;
             this.pictureBox1.TabStop = false;
             // 
