2478789d318dec108a8ced6ce02b193a18d6920d
Edward   Alexander Arroyave Agudelo <edward.arroyave@ithealth.co>
2024-01-18 22:59:41 -0500

    add; orden label input configuracion

--- Codigo/Forms/Config.Designer.cs
+++ Codigo/Forms/Config.Designer.cs
@@ -152,7 +152,7 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Open Sans", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(189)))), ((int)(((byte)(255)))));
-            this.label1.Location = new System.Drawing.Point(34, 21);
+            this.label1.Location = new System.Drawing.Point(41, 20);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(63, 17);
             this.label1.TabIndex = 6;
